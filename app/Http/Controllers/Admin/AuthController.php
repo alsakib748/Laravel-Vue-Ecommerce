ec6e7b8f27a61f8f26a035c4ce48c73876b7cc87
alsakib748 <alsakib748@gmail.com>
2026-01-10 23:57:27 +0600

    User Authenticate API

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -2,13 +2,102 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Validator;
 use App\Models\Role;
 use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+
+    use ApiResponse;
+
+    public function register(Request $request)
+    {
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validation->fails()) {
+            return $this->error($validation->errors()->first(), 400, []);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $customer = Role::where('slug', 'customer')->first();
+
+        $user->roles()->attach($customer);
+
+        return $this->success([
+            'token' => $user->createToken('API Token')->plainTextToken,
+        ]);
+
+
+    }
+
+    public function loginUser(Request $request)
+    {
+
+        $validation = Validator::make($request->all(), [
+
+            'email' => 'required|string|email|exists:users,email',
+            'password' => 'required|string'
+
+        ]);
+
+        // Email not present in DB
+        if ($validation->fails()) {
+            return response()->json([
+                'status' => 400,
+                'message' => $validation->errors()->first(),
+            ]);
+        } else {
+            $cred = array('email' => $request->email, 'password' => $request->password);
+
+            // Right credentials
+            if (Auth::attempt($cred, false)) {
+                if (Auth::User()->hasRole('admin')) {
+                    return response()->json([
+                        'status' => 200,
+                        'message' => 'Login successful',
+                    ]);
+                } else {
+
+                    $user = User::find(Auth::user()->id)->first();
+                    $token = $user->createToken('API Token')->plainTextToken;
+
+                    return response()->json([
+                        'status' => 'success',
+                        'message' => 'Login successful',
+                        'user' => $user,
+                        'token' => $token,
+                    ]);
+
+                    // return response()->json([
+                    //     'status' => 200,
+                    //     'message' => ' Non User',
+                    // ]);
+                }
+            } else {
+                return response()->json([
+                    'status' => 400,
+                    'message' => 'Wrong credentials',
+                ]);
+            }
+
+        }
+
+    }
 
     public function createAdmin()
     {
@@ -23,6 +112,63 @@ class AuthController extends Controller
 
         $user->roles()->attach($admin);
 
+    }
+
+    public function userShow()
+    {
+
+        $user = User::where('id', Auth::user()->id)->first();
+
+        return $this->success([
+            'status' => 'success',
+            'message' => 'User details fetched successfully',
+            'user' => $user,
+        ]);
+
+    }
+
+    public function updateUser(Request $request)
+    {
+
+        $validation = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+        ]);
+
+        if ($validation->fails()) {
+            return $this->error($validation->errors()->first(), 400, []);
+        }
+
+        User::updateOrCreate(
+            ['id' => Auth::user()->id],
+            [
+                'name' => $request->name,
+                // 'email' => $request->email,
+                // 'phone' => $request->phone,
+                // 'address' => $request->address,
+                // 'twitter_link' => $request->twitter_link,
+                // 'fb_link' => $request->fb_link,
+                // 'insta_link' => $request->insta_link,
+                // 'github_link' => $request->github_link,
+                // 'portfolio_link' => $request->portfolio_link,
+            ]
+        );
+
+        return $this->success([
+            'status' => 'success',
+            'message' => 'User details updated successfully',
+            'user' => User::where('id', Auth::user()->id)->first(),
+        ]);
+
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'status' => 'success',
+            'message' => 'Logged out successfully',
+        ]);
     }
 
 }
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AuthController;
+
+Route::post('/register', [AuthController::class, 'register'])->name('register.user');
+
+Route::post('/login', [AuthController::class, 'loginUser'])->name('login.user');
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/showUser', [AuthController::class, 'userShow'])->name('user.show');
+// });
+
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/showUser', [AuthController::class, 'userShow'])->name('user.show');
+
+    Route::post('/updateUser', [AuthController::class, 'updateUser'])->name('update.user');
+
+    Route::post('/logoutUser', [AuthController::class, 'logout'])->name('logout.user');
+
+});
