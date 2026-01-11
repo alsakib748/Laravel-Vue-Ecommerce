71dfe4aa1dfe1e8f5704a9f57f8a8b8e2e25137d
alsakib748 <alsakib748@gmail.com>
2026-01-12 00:59:47 +0600

    Index Page Setup ...

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,12 @@
 import { createWebHistory, createRouter } from "vue-router";
 import test from "./test.vue";
+import Index from "./frontTemplate/Index.vue";
 
 const routes = [
     {
-        name: "test",
+        name: "Index",
         path: "/",
-        component: test,
+        component: Index,
     },
 ];
 
